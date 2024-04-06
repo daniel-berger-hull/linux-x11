5106e6cc049d6f0de6b1a44e01722bc59bee6ed5
daniel-berger-hull <daniel.berger.hull@gmail.com>
2024-04-06 10:23:10 -0400

    Completed the mouse project

--- mouse-click/mouseclick.cpp
+++ mouse-click/mouseclick.cpp
@@ -1,23 +1,43 @@
 /*
-  Very simple example of an X11 Window responding to the user key events
+  Very simple example of an X11 Window responding to the user key abd mouse events
   Will close the window if Escape key is pressed
 */
 
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <cstdio>
+#include <stdio.h>
+#include <string.h>
+
+
+const int WIN_X_POS           = 50;
+const int WIN_Y_POS           = 50;
+const int WIN_WIDTH           = 400;
+const int WIN_HEIGHT          = 300;
+const int WIN_BORDER          = 1;
+
+const int ESCAPE_KEY          = 9;
+
+const int LEFT_MOUSE_BUTTON   = 1;
+const int RIGHT_MOUSE_BUTTON  = 3;
+const int MOUSE_WHEEL_UP      = 4;
+const int MOUSE_WHEEL_DOWN    = 5;
 
 
 
-const int WIN_X_POS     = 50;
-const int WIN_Y_POS     = 50;
-const int WIN_WIDTH     = 200;
-const int WIN_HEIGHT    = 100;
-const int WIN_BORDER    = 1;
 
-const int ESCAPE_KEY    = 9;
+void displayMouseEvent(XEvent e)
+{
+   char buttonClicked[15];
 
+   if (e.xkey.keycode == LEFT_MOUSE_BUTTON) strcpy(buttonClicked, "Left Button");
+   else if (e.xkey.keycode == RIGHT_MOUSE_BUTTON) strcpy(buttonClicked, "Right Button");
+   else if (e.xkey.keycode == MOUSE_WHEEL_UP) strcpy(buttonClicked, "Wheel Up");
+   else if (e.xkey.keycode == MOUSE_WHEEL_DOWN) strcpy(buttonClicked, "Wheel Down");
+   else strcpy(buttonClicked, "Unknown");
 
+   printf("Mouse Click Button EVENT  TYPE IS %d , button is %s, coords are [%d,%d]\n",e.type, buttonClicked, e.xkey.x, e.xkey.y);
+}
 
 int main()
 {
@@ -31,22 +51,12 @@ int main()
 
   int s = DefaultScreen(d);
 
-  /*
-  Window w = XCreateSimpleWindow(d,
-                                 RootWindow(d,s), WIN_X_POS, WIN_Y_POS ,
-                                 WIN_WIDTH, WIN_HEIGHT,
-                                 WIN_BORDER,
-                                 BlackPixel(d,s), WhitePixel(d,s)); */
-
-
-  Window w =XCreateSimpleWindow(d, DefaultRootWindow(d), 0, 0, 300, 600, 5, BlackPixel(d,s), WhitePixel(d,s));
-
-
+  Window w =XCreateSimpleWindow(d, DefaultRootWindow(d), 0, 0, WIN_WIDTH, WIN_HEIGHT, 5, BlackPixel(d,s), WhitePixel(d,s));
 
   XSetStandardProperties(d, w, "Howdy", "Hi", None, NULL, 0, NULL);
 
 
-  //XSelectInput(d,w,KeyPressMask);
+  // Adding the mouse and redraw (expose) event to be monitored in the main event loop...
   XSelectInput(d, w, ExposureMask | ButtonPressMask | KeyPressMask);
 
   XMapWindow(d,w);
@@ -56,23 +66,14 @@ int main()
   while (1)
   {
          XNextEvent(d,&e);
-          printf("EVENT 2: Type is %d , Key Code is %d\n", e.type,e.xkey.keycode);
 
-          if (e.type == KeyPress && e.xkey.keycode == ESCAPE_KEY)
+          if (e.type == KeyPress )
           {
-            break;
+            if (e.xkey.keycode == ESCAPE_KEY) break;
+            else printf("Key EVENT: Type is %d , Key Code is %d\n", e.type,e.xkey.keycode);
           }
 
-           if(e.type==ButtonPress)
-           {
-                printf("EVENT Button\n");
-           }
-
-  /*
-    if ( XPending(d) )
-    {
-
-     } */
+           if(e.type==ButtonPress)           displayMouseEvent(e);
    }
 
 
